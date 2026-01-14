a5df437f4f1926d5e2e161efc578710345d2fbbb
varun00391 <varunsingh2191@gmail.com>
2026-01-14 19:09:10 +0530

    code change

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ logger = logging.getLogger("pdf-api")
 # ------------------------
 # App
 # ------------------------
-app = FastAPI(title="Newwww PDF Extractor API")
+app = FastAPI(title="NewwwwW PDF Extractor API")
 
 
 UPLOAD_DIR = "uploads"
